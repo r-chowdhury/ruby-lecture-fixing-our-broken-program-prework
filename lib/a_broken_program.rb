8ddc2b61ce4ab04c2028066f16d4c993c7bc1fd7
ryhan.chowdhury3@gmail.com <ryhan.chowdhury3@gmail.com>
2018-05-24 19:56:09 +0000

    automatically backed up by learn

--- lib/a_broken_program.rb
+++ lib/a_broken_program.rb
@@ -18,5 +18,5 @@ puts "A language's keywords, like 'if' or 'end' (approximately 43)."
 puts "Literal pieces of data like this very sentence (or String)."
 puts "Finally, barewords, or variables, that are set equal to things."
 
-puts "Anything that isn't one of those will cause an error."
+puts "Anything that isn't one of those will cause an erro"
 
